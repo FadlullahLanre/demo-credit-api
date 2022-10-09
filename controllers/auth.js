3932377cfa97eb701e61d86fcf10b83a619f0851
Fadlullah <fadlulanre@gmail.com>
2022-10-10 00:23:30 +0100

    created controller functions for users and transactions

---
+++ controllers/auth.js
@@ -0,0 +1,103 @@
+const db = require('../config/database')
+const jwt = require('jsonwebtoken')
+const sigupValidator = require('../validator/signup')
+const AppError = require('../utils/appError')
+const loginValidator = require('../validator/login')
+const { promisify } = require('util')
+const bycrpt = require('bcrypt')
+
+const signToken = id => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    })
+}
+
+const createToken = async (user, statusCode, res) => {
+    const token = signToken(user[0].id)
+    user[0].password = undefined
+    user[0].pin = undefined
+    res.status(statusCode).json({
+        status: 'success',
+        token,
+        data: {
+            user
+        }
+    })
+}
+
+const acctNumber = () => {
+    let result = ''
+    let length = 10 // Customize the length here.
+    for (let i = length; i > 0; --i) result += `${Math.floor(Math.random() * 10)}`
+
+    return result
+
+}
+
+const signup = async (req, res, next) => {
+    try {
+        const value = await sigupValidator.validateAsync(req.body)
+        const accountNo = acctNumber()
+        value.password = await bycrpt.hash(value.password, 12)
+
+        await db('users').insert({
+            name: value.name,
+            email: value.email,
+            password: value.password,
+            account_No: accountNo
+        })
+        const newUser = await db.select().from("users").where({ email: value.email })
+        createToken(newUser, 201, res)
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+const login = async (req, res, next) => {
+    try {
+        const value = await loginValidator.validateAsync(req.body)
+        if (!value.password || !value.email) {
+            throw new AppError("Please provide an email and password", 400)
+        }
+
+        const user = await db.select().from('users').where({ email: value.email })
+
+        if (!user || !await bycrpt.compare(value.password, user[0].password)) {
+            throw new AppError('incorrect password or email', 401)
+        }
+        createToken(user, 200, res)
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+const protect = async (req, res, next) => {
+    try {
+        let token
+        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1]
+        }
+        if (!token) {
+            return next(new AppError('You are not login, Please login to get access', 401))
+        }
+        let decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+
+        const currentUser = await db.select().from('users').where({ id: decoded.id })
+
+        if (!currentUser) {
+            return next(new AppError('The user belonging to this token, no longer exist', 401))
+        }
+        req.user = currentUser
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {
+    signup,
+    protect,
+    login
+}
---
+++ controllers/user.js
@@ -0,0 +1,108 @@
+const AppError = require('../utils/appError')
+const db = require('../config/database')
+const { deposit, withdrawal, transfer, GetAllUser, deleteAllUser } = require('../services/user')
+const { bankPin } = require('../services/pin')
+
+exports.getUser = async (req, res, next) => {
+    try {
+        const user = req.user
+        user.Password = undefined
+        user.Pin = undefined
+        res.status(200).json({
+            status: "successful",
+            data: {
+                user
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.createPin = async (req, res, next) => {
+    try {
+        await bankPin(req.user, req.body)
+        res.status(201).json({
+            status: "successful",
+            message: "Pin has been created"
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+exports.deposit = async (req, res, next) => {
+    try {
+        const user = await deposit(req.user, req.body.deposit)
+        res.status(200).json({
+            status: "successful",
+            data: {
+                user
+            }
+        })
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.withdraw = async (req, res, next) => {
+    try {
+        const user = await withdrawal(req.user, req.body.withdrawal, req.body.pin)
+        res.status(200).json({
+            status: "successful",
+            data: {
+                user
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+exports.transfer = async (req, res, next) => {
+    try {
+        const accountNo = req.body.acct_no
+        const otherUser = await db.select().from('users').where({ account_no: accountNo })
+        // console.log(otherUser)
+        const user = await transfer(req.user, otherUser, req.body.amount, req.body.pin)
+
+        res.status(200).json({
+            status: "successful",
+            data: {
+                user
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+exports.AllUser = async (req, res, next) => {
+    try {
+        const users = await GetAllUser()
+        res.status(200).json({
+            status: "successful",
+            data: {
+                users
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.deleteUser = async (req, res, next) => {
+    try {
+        await deleteAllUser()
+        res.status(204).json({
+            status: "successful",
+            message: "All users have been deleted"
+        })
+    } catch (error) {
+        next(error)
+    }
+}
